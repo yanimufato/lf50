23d0c737e86a2814cb0f76ba77f000a8b9c6d6e4
Yani Mufato <yf.mufato@gmail.com>
2022-06-06 19:46:35 -0300

    primer desafio

---
+++ JS/main.js
@@ -0,0 +1,26 @@
+/*let usuarioIngresado = prompt("Ingrese su usuario")
+let passwordIngresado = prompt("Ingrese su contraseña")
+
+if ((usuarioIngresado != "") && (passwordIngresado != "")){
+    console.log("Bienvenido", usuarioIngresado)
+}*/
+
+
+debugger
+function stockAutor () {
+    let nombreIngresado = prompt("Ingrese nombre del autor")
+    console.log ("Resultado: ", calcularStock (nombreIngresado))
+}
+function calcularStock (nombreIngresado){
+    switch (autor) {
+        case "Galeano":
+            alert("Tenemos al autor seleccionado")
+        case "Storni":
+            alert("Tenemos al autor seleccionado")
+        case "Cortazar":
+            alert("Tenemos al autor seleccionado")
+        default:
+            alert("No tenemos al autor seleccionado")
+        break               
+    }
+}
