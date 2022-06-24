9c4711f79892eb451ceb0ffe24145ac8c388eb6f
Yani Mufato <yf.mufato@gmail.com>
2022-06-24 17:07:15 -0300

    armado archivo html

---
+++ JS/variables.js
@@ -0,0 +1,5 @@
+const librosArte = document.querySelector("#librosArte")
+const librosPolitica = document.querySelector("#librosPolitica")
+const librosHistoria = document.querySelector("#librosHistoria")
+
+const btnComprar = document.querySelector("btnComprar")
